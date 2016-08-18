080da83d2dc5f57f1e90cf1094901ccfadfe31d3
Kaustuv Chaudhuri <kaustuv.chaudhuri@inria.fr>
2016-08-18 17:00:35 +0200

    coq: half-hearted attempt at the merge library

--- coq/MS.v
+++ coq/MS.v
@@ -12,7 +12,7 @@ Inductive adj : list T -> T -> list T -> Prop :=
   | adj_same : forall {X L}, adj L X (X :: L)
   | adj_diff : forall {X Y K L}, adj K X L -> adj (Y :: K) X (Y :: L).
 
-Hint Resolve adj_same adj_diff : adj.
+Hint Resolve adj_same adj_diff : ms.
 
 Theorem adj_swap :
   forall {X Y : T} {J K L : list T},
@@ -47,7 +47,7 @@ Inductive perm : list T -> list T -> Prop :=
   | perm_nil : perm nil nil
   | perm_adj : forall {X KK K LL L}, adj KK X K -> adj LL X L -> perm KK LL -> perm K L.
 
-Hint Resolve perm_nil perm_adj : perm.
+Hint Resolve perm_nil perm_adj : ms.
 
 Theorem perm_refl : forall {L}, perm L L.
 Proof.
@@ -140,14 +140,14 @@ Ltac adjify_one :=
       assert (adj K X L) ; [ rewrite Heq ; exact adj_same | ]
   end.
 
-Ltac ms_simpl := repeat (intros ; eauto with adj perm ; (adj_one || perm_one (* || adjify_one *))).
+Ltac ms_simpl := repeat (intros ; eauto with ms ; (adj_one || perm_one (* || adjify_one *))).
 
 Theorem perm_trans : forall {J K L}, perm J K -> perm K L -> perm J L.
 Proof.
   intros until K ; dependent induction K generalizing J ; ms_simpl.
 Qed.
 
-Theorem adj_same_source : forall J X L1 L2,
+Theorem adj_same_source : forall {J X L1 L2},
   adj J X L1 ->
   adj J X L2 ->
   perm L1 L2.
@@ -180,7 +180,7 @@ Proof.
     - destruct (IHadj K1 H4) as [ ( Heq & Hperm ) | ( KK & HH ) ] ; now ms_simpl.
 Qed.
 
-Theorem perm_invert : forall X J K JJ KK,
+Theorem perm_invert : forall {X J K JJ KK},
   perm J K ->
   adj JJ X J ->
   adj KK X K ->
@@ -199,22 +199,18 @@ Proof.
                  (perm_trans (perm_adj HKU HKUU IHperm) (adj_same_result HUUL H3))).
 Qed.
 
-Theorem adj_perm_result : forall J K X JJ,
-  perm J K ->
+Theorem adj_perm_result : forall {J K JJ X},
   adj JJ X J ->
+  perm J K ->
   exists KK, adj KK X K /\ perm JJ KK.
 Proof.
-  intros until 1. dependent induction H generalizing JJ ; intros.
-  * now ms_simpl.
-  * destruct (adj_same_result_diff H2 H) as [ ( Heq & Hperm ) | ( KKK & Hadj ) ].
-    - subst X ; exists LL ; split ; [ assumption | refine (perm_trans Hperm H1) ].
-    - destruct (adj_swap Hadj H) as [ U ( HK3U & HUK ) ].
-      destruct (IHperm _ Hadj) as [ UU ( HUULL & Hperm ) ].
-      destruct (adj_swap HUULL H0) as [ Z ( HUUZ & HZL ) ].
-      cut (perm JJ Z) ; [ eauto | refine (perm_trans (adj_same_result H2 HUK) _) ; now ms_simpl ].
+  intros until 1. dependent induction H generalizing K ; intros ; ms_simpl.
+  destruct (IHadj _ H1) as [ KK ( Hadj & Hperm ) ].
+  destruct (adj_swap Hadj H0) as [ U ( HKKU & HUK ) ].
+  ms_simpl.
 Qed.
 
-Theorem adj_perm_source : forall J K X L,
+Theorem adj_perm_source : forall {J K X L},
   perm J K ->
   adj J X L ->
   exists LL, adj K X LL /\ perm L LL.
@@ -228,6 +224,176 @@ Module Merge (Import CC : CARRIER).
 
 Include Perm CC.
 
-End Merge.
+Inductive merge : list T -> list T -> list T -> Prop :=
+  | merge_nil : merge nil nil nil
+  | merge_left : forall {X JJ J K LL L}, merge JJ K LL -> adj JJ X J -> adj LL X L -> merge J K L
+  | merge_right : forall {X J KK K LL L}, merge J KK LL -> adj KK X K -> adj LL X L -> merge J K L.
 
-Print Merge.
+Hint Resolve merge_nil merge_left merge_right : ms.
+
+Theorem perm_merge_1 : forall {J K L JJ},
+  merge J K L ->
+  perm J JJ ->
+  merge JJ K L.
+Proof.
+  intros until 1. dependent induction H generalizing JJ ; intros ; ms_simpl.
+  destruct (adj_perm_result H0 H2) as [ U ( Hadj & Hperm ) ].
+  specialize (IHmerge _ Hperm). eauto with ms.
+Qed.
+
+Theorem perm_merge_2 : forall {J K L KK},
+  merge J K L ->
+  perm K KK ->
+  merge J KK L.
+Proof.
+  intros until 1. dependent induction H generalizing KK ; intros ; ms_simpl.
+  destruct (adj_perm_result H0 H2) as [ U ( Hadj & Hperm ) ].
+  specialize (IHmerge _ Hperm). eauto with ms.
+Qed.
+
+Theorem perm_merge_3 : forall {J K L LL},
+  merge J K L ->
+  perm L LL ->
+  merge J K LL.
+Proof.
+  intros until 1. dependent induction H generalizing LL ; intros ; ms_simpl ;
+  destruct (adj_perm_result H1 H2) as [ U ( Hadj & Hperm ) ] ;
+  specialize (IHmerge _ Hperm) ; eauto with ms.
+Qed.
+
+Theorem merge_sym : forall {J K L},
+  merge J K L ->
+  merge K J L.
+Proof.
+  induction 1 ; eauto with ms.
+Qed.
+
+Theorem merge_unadj_1 : forall {X JJ J K L},
+  merge J K L ->
+  adj JJ X J ->
+  exists LL, adj LL X L /\ merge JJ K LL.
+Proof.
+  intros until 1. dependent induction H generalizing JJ ; intros ; ms_simpl.
+  * destruct (adj_same_result_diff H2 H0) as [ ( Heq & Hperm ) | ( KK & Hadj ) ].
+    - subst X0. exists LL. split ; [ assumption | exact (perm_merge_1 H (perm_sym Hperm)) ].
+    - destruct (IHmerge _ Hadj) as [ U ( HU & Hmerge ) ].
+      destruct (adj_swap HU H1) as [ V ( HUV & HVL ) ].
+      exists V ; split ; [ assumption | ].
+      destruct (adj_swap Hadj H0) as [ W ( HKKW & HWJ ) ].
+      refine (perm_merge_1 _ (adj_same_result HWJ H2)). eauto with ms.
+  * destruct (IHmerge _ H2) as [ U ( HU & Hmerge ) ].
+    destruct (adj_swap HU H1) as [ V ( HUV & HVL ) ].
+    assert (merge JJ K V) ; eauto with ms.
+Qed.
+
+Theorem merge_unadj_2 : forall {X J KK K L},
+  merge J K L ->
+  adj KK X K ->
+  exists LL, adj LL X L /\ merge J KK LL.
+Proof.
+  intros.
+  destruct (merge_unadj_1 (merge_sym H) H0) as [ U ( Hadj & Hmerge ) ].
+  pose (HH := merge_sym Hmerge).
+  eauto with ms.
+Qed.
+
+Theorem merge_unadj_3 : forall {X J K L LL},
+  merge J K L ->
+  adj LL X L ->
+  (exists JJ, adj JJ X J /\ merge JJ K LL) \/
+  (exists KK, adj KK X K /\ merge J KK LL).
+Proof.
+  intros until 1. dependent induction H generalizing LL ; intros ; ms_simpl ; 
+  destruct (adj_same_result_diff H1 H2) as [ ( Heq & Hperm ) | ( U & Hadj ) ].
+  * left ; subst X0 ; exists JJ ; split ; [ assumption | exact (perm_merge_3 H Hperm) ].
+  * destruct (adj_swap Hadj H2) as [ V ( HUV & HVL ) ].
+    destruct (adj_perm_result HUV (adj_same_result HVL H1)) as [ W ( HadjW & HpermW ) ].
+    destruct (IHmerge _ HadjW) as [ (Z & HadjZ & HmergeZ ) | ( Z & HadjZ & HmergeZ ) ].
+    - destruct (adj_swap HadjZ H0) as [ ZZ ( HtoZZ & HfromZZ ) ].
+      left ; exists ZZ ; split. assumption.
+      pose (HH := perm_merge_3 HmergeZ (perm_sym HpermW)). eauto with ms.
+    - right ; exists Z ; split. assumption.
+      destruct (adj_swap HadjW H1) as [ ZZ ( HtoZZ & HfromZZ ) ].
+      refine (perm_merge_3 _ (adj_same_result HfromZZ H2)). eauto with ms.
+  * right ; subst X0 ; exists KK ; split ; [ assumption | exact (perm_merge_3 H Hperm) ].
+  * destruct (adj_swap Hadj H2) as [ V ( HUV & HVL ) ].
+    destruct (adj_perm_result HUV (adj_same_result HVL H1)) as [ W ( HadjW & HpermW ) ].
+    destruct (IHmerge _ HadjW) as [ (Z & HadjZ & HmergeZ ) | ( Z & HadjZ & HmergeZ ) ].
+    - left ; exists Z ; split. assumption.
+      destruct (adj_swap HadjW H1) as [ ZZ ( HtoZZ & HfromZZ ) ].
+      refine (perm_merge_3 _ (adj_same_result HfromZZ H2)). eauto with ms.
+    - destruct (adj_swap HadjZ H0) as [ ZZ ( HtoZZ & HfromZZ ) ].
+      right ; exists ZZ ; split. assumption.
+      pose (HH := perm_merge_3 HmergeZ (perm_sym HpermW)). eauto with ms.
+Qed.
+
+Theorem merge_invert_1 : forall {X JJ J K LL L},
+  merge J K L ->
+  adj JJ X J ->
+  adj LL X L ->
+  merge JJ K LL.
+Proof.
+  intros.
+  destruct (merge_unadj_1 H H0) as [ U ( Hadj & Hmerge ) ].
+  exact (perm_merge_3 Hmerge (adj_same_result Hadj H1)).
+Qed.
+
+Theorem merge_invert_2 : forall {X J KK K LL L},
+  merge J K L ->
+  adj KK X K ->
+  adj LL X L ->
+  merge J KK LL.
+Proof.
+  intros.
+  destruct (merge_unadj_2 H H0) as [ U ( Hadj & Hmerge ) ].
+  exact (perm_merge_3 Hmerge (adj_same_result Hadj H1)).
+Qed.
+
+Theorem merge_move_12 : forall {X JJ J KK K L},
+  adj JJ X J ->
+  adj KK X K ->
+  merge J KK L ->
+  merge JJ K L.
+Proof.
+  intros.
+  destruct (merge_unadj_1 H1 H) as [ U ( Hadj & Hmerge ) ].
+  eauto with ms.
+Qed.
+
+Theorem merge_move_21 : forall {X JJ J KK K L},
+  adj JJ X J ->
+  adj KK X K ->
+  merge JJ K L ->
+  merge J KK L.
+Proof.
+  intros.
+  destruct (merge_unadj_2 H1 H0) as [ U ( Hadj & Hmerge ) ].
+  eauto with ms.
+Qed.
+
+Theorem merge_nil_perm : forall {K L},
+  merge nil K L -> perm K L.
+Proof.
+  intros until 1. dependent induction H ; ms_simpl.
+Qed.
+
+Theorem merge_assoc : forall {J K L JK KL JKL1 JKL2},
+  merge J K JK -> merge K L KL ->
+  merge J KL JKL1 -> merge JK L JKL2 ->
+  perm JKL1 JKL2.
+Proof.
+  intros until 1. dependent induction H generalizing L KL JKL1 JKL2 ; intros.
+  - exact (perm_trans (perm_sym (merge_nil_perm H0))
+             (perm_trans (perm_sym (merge_nil_perm H)) (merge_nil_perm H1))).
+  - destruct (merge_unadj_1 H3 H0) as [ U ( HadjU & HmergeU ) ].
+    destruct (merge_unadj_1 H4 H1) as [ V ( HadjV & HmergeV ) ].
+    specialize (IHmerge _ _ _ _ H2 HmergeU HmergeV).
+    eauto with ms.
+  - destruct (merge_unadj_1 H2 H0) as [ U ( HadjU & HmergeU ) ].
+    destruct (merge_unadj_1 H4 H1) as [ V ( HadjV & HmergeV ) ].
+    destruct (merge_unadj_2 H3 HadjU) as [ W ( HadjW & HmergeW ) ].
+    specialize (IHmerge _ _ _ _ HmergeU HmergeW HmergeV).
+    eauto with ms.
+Qed.
+
+End Merge.
